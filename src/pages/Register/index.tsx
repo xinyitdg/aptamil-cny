241b9ea78b580ac2aed7678f6f99ca5c4ad65066
Xin Yi <xinyi@thedgroup.com.my>
2024-12-03 17:24:23 +0800

    new background and register page

---
+++ src/pages/Register/index.tsx
@@ -0,0 +1,113 @@
+import React, { useState } from 'react';
+
+import { useLocation } from 'react-router-dom';
+
+import cnyBody from '../../assets/images/cny-body.png';
+import cnyTop from '../../assets/images/cny-top.png';
+import AuthForm from '../../components/AuthForm';
+import Checkbox from '../../components/Checkbox';
+import Header from '../../components/Header';
+
+const Register: React.FC = () => {
+  const location = useLocation();
+  const [termsChecked, setTermsChecked] = useState(false);
+
+  const handleTermsChange = (checked: boolean) => {
+    setTermsChecked(checked);
+  };
+
+  return (
+    <div id="page" className="overflow-y-auto">
+      <div className="absolute flex justify-between w-full">
+        <Header />
+      </div>
+      <div className="relative z-[2]">
+        <img src={cnyTop} alt="gif" className="w-full h-full" />
+      </div>
+
+      <div className="relative -mt-[115px] overflow-hidden z-[3]">
+        <img
+          src={cnyBody}
+          alt="main-bg"
+          className="absolute w-full min-h-screen top-0 left-0"
+        />
+
+        <div className="pt-[110px] flex flex-col items-center justify-between relative">
+          <p className="heading-1 border-b-[3px] border-[#FFDB20] py-2.5 px-[50px]">
+            Sign Up
+          </p>
+          <AuthForm
+            formConfig={{
+              fields: [
+                {
+                  name: 'name',
+                  type: 'name',
+                  placeholder: 'Full Name*',
+                  inputGroupClass: 'input-signup',
+                  inputDivClass: 'input-div',
+                  inputClass: 'input-field',
+                  required: true,
+                },
+                {
+                  name: 'email',
+                  type: 'email',
+                  placeholder: 'Email*',
+                  inputGroupClass: 'input-signup',
+                  inputDivClass: 'input-div',
+                  inputClass: 'input-field',
+                  required: true,
+                },
+              ],
+              btnText: 'REGISTER',
+              authFormClass: 'auth-form',
+            }}
+            additionalFields={{
+              path: location.pathname,
+              params: location.search,
+            }}
+          >
+            <div className="mb-4 w-full">
+              <Checkbox
+                label={
+                  <div>
+                    <span>By submitting this form, I hereby agree to the </span>
+                    <a href="/terms" className="underline font-bold">
+                      Terms and Conditions
+                    </a>
+                    <span>
+                      {' '}
+                      and consent to the collection and processing of my personal data by
+                      Danone Specialized Nutrition (Malaysia) Sdn. Bhd. in accordance with
+                      the ​
+                    </span>
+                    <a href="/privacy" className="underline font-bold">
+                      Privacy Policy
+                    </a>
+                  </div>
+                }
+                initialChecked={termsChecked}
+                onChange={handleTermsChange}
+              />
+              <Checkbox
+                label={
+                  <div>
+                    <span>
+                      I hereby consent to receive AptamilTM KID information via
+                      mail/SMS/phone/WhatsApp/Email. Collected data can also be used for
+                      customer service purpose.
+                    </span>
+                  </div>
+                }
+                initialChecked={termsChecked}
+                onChange={handleTermsChange}
+              />
+            </div>
+          </AuthForm>
+          <div className="footer-div"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
--- src/pages/Verify/index.tsx
+++ src/pages/Verify/index.tsx
@@ -1,18 +1,77 @@
+import { useState } from 'react';
+
+import OtpInput from 'react-otp-input';
+
+import cnyBody from '../../assets/images/cny-body.png';
+import cnyTop from '../../assets/images/cny-top.png';
+import ButtonComponent from '../../components/ButtonComponent';
 import Header from '../../components/Header';
 
 const Verify: React.FC = () => {
+  const [otp, setOtp] = useState('');
+  const [loading, setLoading] = useState(false);
+  const handleSubmit = () => {
+    setLoading(true);
+    try {
+      console.log(otp);
+    } catch (error) {
+      console.log(error);
+      alert('Failed to');
+    }
+  };
+
   return (
-    <div className="main-bg w-full h-full mx-auto relative overflow-auto flex flex-col">
-      <Header />
-      <div className="login-bottom flex flex-col items-center justify-between relative">
-        <p className="heading-1 border-b-[3px] border-[#FFDB20] py-2.5 px-[50px]">
-          OTP Verication
-        </p>
-        <p className="heading-2 p-4 text-center">
-          Please enter the verification code sent to
-          <br />
-          <strong>0123456789</strong>
-        </p>
+    <div id="page" className="overflow-y-auto">
+      <div className="absolute flex justify-between w-full">
+        <Header />
+      </div>
+      <div className="relative z-[2]">
+        <img src={cnyTop} alt="gif" className="w-full h-full" />
+      </div>
+
+      <div className="relative -mt-[115px] overflow-hidden z-[3]">
+        <img
+          src={cnyBody}
+          alt="main-bg"
+          className="absolute w-full min-h-screen top-0 left-0"
+        />
+
+        <div className="pt-[110px] flex flex-col items-center justify-between relative w-[90%] mx-auto">
+          <p className="heading-1 border-b-[3px] border-[#FFDB20] py-2.5 px-[50px]">
+            OTP Verication
+          </p>
+          <p className="heading-2 p-4 text-center">
+            Please enter the verification code sent to
+            <br />
+            <strong>0123456789</strong>
+          </p>
+          <div className="pb-3">
+            <OtpInput
+              value={otp}
+              onChange={setOtp}
+              numInputs={4}
+              renderSeparator={
+                <span className="text-white">&nbsp;&nbsp;&nbsp;&nbsp; </span>
+              }
+              renderInput={props => <input {...props} />}
+              containerStyle="w-[284px] mx-auto flex justify-center items-center"
+              inputStyle="bg-transparent border-b-[3px] border-b-[#FFFFFF] !w-[62px] h-[58px] gotham-bold text-white text-[32px]"
+            />
+          </div>
+          <p className="text-[16px] text-white">00:45</p>
+          <p className="text-[16px] text-white">Resend OTP</p>
+        </div>
+        <div className="footer-div">
+          <div className="absolute z-40 w-full mx-auto text-center top-[140px]">
+            <ButtonComponent
+              buttonText="VERIFY"
+              buttonType="submit"
+              loading={loading}
+              buttonFunction={() => handleSubmit()}
+              buttonClass="button-component"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
