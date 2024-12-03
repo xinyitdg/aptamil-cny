61344001717912d0f5da25378f880e5f2edbd42c
Xin Yi <xinyi@thedgroup.com.my>
2024-12-03 17:26:28 +0800

    home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,98 @@
+import { useState } from 'react';
+
+import { useLocation } from 'react-router-dom';
+
+import cnyBody from '../../assets/images/cny-body.png';
+import cnyTop from '../../assets/images/cny-top.png';
+import matchWin from '../../assets/images/match-and-win.png';
+import playRedeem from '../../assets/images/play-and-redeem.png';
+import ButtonComponent from '../../components/ButtonComponent';
+import Footer from '../../components/Footer';
+import Header from '../../components/Header';
+
+const Home: React.FC = () => {
+  const location = useLocation();
+
+  return (
+    <div id="page" className="overflow-y-auto">
+      <div className="absolute flex justify-between w-full">
+        <Header />
+      </div>
+      <div className="relative z-[2]">
+        <img src={cnyTop} alt="gif" className="w-full h-full" />
+      </div>
+
+      <div className="relative -mt-[115px] overflow-hidden z-[3] pb-[73px]">
+        <img
+          src={cnyBody}
+          alt="main-bg"
+          className="absolute w-full min-h-screen top-0 left-0"
+        />
+        <div className="pt-[130px] pb-[20px] flex flex-col items-center justify-between relative z-[2]">
+          <div className="grid flex-grid grid-cols-2 gap-2 w-[90%] mx-auto">
+            <div className="relative">
+              <img src={playRedeem} className="h-[191px] mx-auto"></img>
+            </div>
+            <div className="relative">
+              <img src={matchWin}></img>
+            </div>
+          </div>
+        </div>
+        <div className="footer-div">
+          <div className="grid flex-grid grid-cols-2 gap-2 w-[90%] mx-auto">
+            <div className="relative">
+              <div className="z-40 w-full mx-auto text-center top-[140px]">
+                <ButtonComponent
+                  buttonText="GUARANTEED REWARDS"
+                  buttonType="submit"
+                  buttonClass="blue-button"
+                />
+              </div>
+            </div>
+            <div className="relative">
+              <div className="z-40 w-full mx-auto text-center top-[140px]">
+                <ButtonComponent
+                  buttonText="GRAND PRIZES"
+                  buttonType="submit"
+                  buttonClass="blue-button"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="relative z-40 w-full mx-auto text-center my-3">
+            <ButtonComponent
+              buttonText="UPLOAD RECEIPT"
+              buttonType="submit"
+              buttonClass="button-component"
+            />
+          </div>
+          <div className="grid flex-grid grid-cols-2 gap-2 w-[90%] mx-auto">
+            <div className="relative">
+              <div className="z-40 w-full mx-auto text-center top-[140px]">
+                <ButtonComponent
+                  buttonText="TRACK MY SUBMISSION"
+                  buttonType="submit"
+                  buttonClass="home-button"
+                />
+                <p className="text-white underline mt-2">Terms & Conditions</p>
+              </div>
+            </div>
+            <div className="relative">
+              <div className="z-40 w-full mx-auto text-center top-[140px]">
+                <ButtonComponent
+                  buttonText="HOW TO JOIN"
+                  buttonType="submit"
+                  buttonClass="home-button"
+                />
+                <p className="text-white underline mt-2">Privacy Policy</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -5,8 +5,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ReactHelmet from '../components/ReactHelmet';
 import Join from '../pages/Join';
 import Login from '../pages/Login';
-import Signup from '../pages/Signup';
+import Register from '../pages/Register';
 import Verify from '../pages/Verify';
+import Home from '../pages/Home';
 
 const Router: React.FC = () => {
   return (
@@ -15,8 +16,9 @@ const Router: React.FC = () => {
       <Routes>
         <Route path={'/login'} element={<Login />} />
         <Route path={'/verify'} element={<Verify />} />
-        <Route path={'/signup'} element={<Signup />} />
+        <Route path={'/register'} element={<Register />} />
         <Route path={'/join'} element={<Join />} />
+        <Route path={'/home'} element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
